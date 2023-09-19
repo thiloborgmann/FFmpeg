23268e08c1aec77d24f04fc78f8fa83932436ca7
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2023-09-19 21:57:22 +0200

    avcodec/vp9: Switch to ProgressFrames
    This already fixes a race in the vp9-encparams test. In this test,
    side data is added to the current frame after having been decoded
    (and therefore after ff_thread_finish_setup() has been called).
    Yet the update_thread_context callback called ff_thread_ref_frame()
    and therefore av_frame_ref() with this frame as source frame and
    the ensuing read was unsynchronised with adding the side data,
    i.e. there was a data race.
    
    By switching to the ProgressFrame API the implicit av_frame_ref()
    is removed and the race fixed except if this frame is later reused by
    a show-existing-frame which uses an explicit av_frame_ref().
    The vp9-encparams test does not cover this, so this commit
    already fixes all the races in this test.
    
    This decoder kept multiple references to the same ThreadFrames
    in the same context and therefore had lots of implicit av_frame_ref()
    even when decoding single-threaded. This incurred lots of small
    allocations: When decoding an ordinary 10s video in single-threaded
    mode the number of allocations reported by Valgrind went down
    from 57,814 to 20,908; for 10 threads it went down from 84,223 to
    21,901.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/dxva2_vp9.c
+++ libavcodec/dxva2_vp9.c
@@ -80,7 +80,7 @@ static int fill_picture_parameters(const AVCodecContext *avctx, AVDXVAContext *c
     pp->Reserved8Bits = 0;
 
     for (i = 0; i < 8; i++) {
-        if (h->refs[i].f->buf[0]) {
+        if (h->refs[i].f) {
             fill_picture_entry(&pp->ref_frame_map[i], ff_dxva2_get_surface_index(avctx, ctx, h->refs[i].f), 0);
             pp->ref_frame_coded_width[i]  = h->refs[i].f->width;
             pp->ref_frame_coded_height[i] = h->refs[i].f->height;
@@ -90,7 +90,7 @@ static int fill_picture_parameters(const AVCodecContext *avctx, AVDXVAContext *c
 
     for (i = 0; i < 3; i++) {
         uint8_t refidx = h->h.refidx[i];
-        if (h->refs[refidx].f->buf[0])
+        if (h->refs[refidx].f)
             fill_picture_entry(&pp->frame_refs[i], ff_dxva2_get_surface_index(avctx, ctx, h->refs[refidx].f), 0);
         else
             pp->frame_refs[i].bPicEntry = 0xFF;
